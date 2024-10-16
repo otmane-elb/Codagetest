cee2071ad0dccdf10d4061ab34c1ac9f9fce28d3
oelbaghzaoui <otmane.elbaghzaoui@gmail.com>
2024-10-16 23:12:48 +0100

    first commit

---
+++ android/app/src/main/kotlin/com/example/codagetest/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.codagetest
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:codagetest/views/screens/home/home_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Codage Test',
+      theme: ThemeData(
+        useMaterial3: true,
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
